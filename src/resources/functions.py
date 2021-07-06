0877603a7627732ab9f54ca0d63d198dcf95812c
Richard <richarddavidrv13@gmail.com>
2021-07-06 12:11:23 -0400

    Added async functions on different folder

--- src/resources/functions.py
+++ src/resources/functions.py
@@ -19,7 +19,7 @@ def getTableRows(session, code, table, scope, lower, upper):
 
 async def miningPower(account):
     async with aiohttp.ClientSession() as session:
-        #VEMOS TODAS LAS POOLS
+        #ALL POOLS
         send = {
         "json": True,
         "code": "s.rplanet",
@@ -32,7 +32,7 @@ async def miningPower(account):
         poolinfo = requests.post(URL_API + "/get_table_rows", json=send).json()
         mining = {}
         tasks = []
-        #VEMOS TODAS LAS ROW EN POOLINFO
+        #ROWS AT POOLINFO
         for row in poolinfo['rows']:
             tasks.append(getTableRows(session, "s.rplanet", "accounts", row['id'], account, account))
         responses = await asyncio.gather(*tasks)
@@ -40,13 +40,13 @@ async def miningPower(account):
             resp = await response.json()
             if resp['rows']:
                 mining[poolinfo['rows'][responses.index(response)]['id']] = resp
-        #VEMOS TODA LA DATA EN POOL INFO PARA VER SI ESTA EN EL MINING
+        #POOLINFO DATA IN MINING DATA
         for data in poolinfo['rows']:
             if data['id'] in mining:
                 fraction = float(data['fraction'].split(' ')[0])
                 mining[data['id']]['fraction'] = fraction
                 mining[data['id']]['pool'] = int(data['staked'])
-        #CALCULAMOS EL PODER DE MINADO DE CADA UNO
+        #CALCULATING MINING POWER
         for data in mining:
             if data == 's.rplanet':
                 staked = mining[data]['rows'][0]['staked']
